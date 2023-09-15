de545518ae2554f4f62fc9670d58f83228f62024
Khyn Antoque <khynantoque@gmail.com>
2023-09-15 22:47:58 +0800

    created admin models db tables and fields

--- chismis/chismis/settings.py
+++ chismis/chismis/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'server'
 ]
 
 MIDDLEWARE = [
@@ -112,3 +114,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.Account'
