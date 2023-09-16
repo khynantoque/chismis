0933b7fcb2f3976bcd8a1b46c667214e80b5bc39
Khyn Antoque <khynantoque@gmail.com>
2023-09-16 22:45:06 +0800

    Added filter by server id

--- chismis/server/views.py
+++ chismis/server/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError, AuthenticationFailed
 from server.serializer import ServerSerializer
 from .models import Server
 
@@ -14,6 +15,10 @@ class ServerListViewSet(viewsets.ModelViewSet):
         category = request.query_params.get('category')
         qty = request.query_params.get('qty')
         by_user = request.query_params.get('by_user') == 'True'
+        by_serverid = request.query_params.get('by_serverid')
+        
+        if not request.user.is_authenticated:
+            raise AuthenticationFailed(detail='Authentication is required!')
         
         if category:
             self.queryset = self.queryset.filter(category__name=category)
@@ -24,6 +29,14 @@ class ServerListViewSet(viewsets.ModelViewSet):
         
         if qty:
             self.queryset = self.queryset[:int(qty)]
+            
+        if by_serverid:
+            try:
+                self.queryset = self.queryset.filter(id=int(by_serverid))
+                if not self.queryset.exists():
+                    raise ValidationError(detail=f'Server with ID of {by_serverid} is not found!')
+            except ValueError:
+                raise ValidationError(detail=f'Server value error!')
         
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
