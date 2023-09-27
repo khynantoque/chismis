31be423f0a041009ee3087e004ac8dd3def92c07
Khyn Antoque <khynantoque@gmail.com>
2023-09-27 17:18:11 +0800

    Created CRUD Hook

---
+++ reactchismis/src/hooks/useCrud.ts
@@ -0,0 +1,41 @@
+import { responsiveFontSizes } from '@mui/material';
+import { useState } from "react"
+import useAxiosWithInterceptor from "../helpers/jwtinterceptor"
+import { BASE_URL } from '../config';
+
+interface IuseCrud<T> {
+    dataCRUD: T[]
+    fetchData: () => Promise<void>
+    error: Error | null
+    isLoading: boolean
+}
+
+const useCrud = <T>(initialData: T[], apiURL: string): IuseCrud<T> => {
+    const jwtAxios = useAxiosWithInterceptor()
+    const [dataCRUD, setDataCRUD] = useState<T[]>(initialData)
+    const [error, setError] = useState<Error | null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
+
+    const fetchData =async () => {
+        setIsLoading(true)
+        try {
+            const response = await jwtAxios.get(`${BASE_URL}${apiURL}`, {})
+            const {data} = response
+            setDataCRUD(data)
+            setError(null)
+            setIsLoading(false)
+            return data
+        } catch (error: any) {
+            if (error.response && error.response.data === 400) {
+                setError(new Error('400'))
+            }
+            
+            setIsLoading(false)
+            throw error;
+        }
+    }
+
+    return {fetchData, dataCRUD, error, isLoading}
+}
+
+export default useCrud;
